38c9b3195efbd16610f78aa2d49a961ac9ebb8ae
Olavo <olavo.santos@skipthedishes.ca>
2021-04-03 15:33:29 -0500

    added custom BS port with env variable

--- .config/dev/sync.js
+++ .config/dev/sync.js
@@ -2,6 +2,7 @@ const bs = require('browser-sync');
 const { cwd, wait } = require('../utilities');
 
 wait(1100).then(() => bs({
+  port: process.env.PORT ? Number(process.env.PORT) : 3000,
   files: [
     {
       match: [cwd('.micra/**/*.js')],
