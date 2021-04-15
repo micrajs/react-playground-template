5cf22736f3fad7248f743a09b52338eca3abf88b
Olavo <olavo.santos@skipthedishes.ca>
2021-04-15 13:49:47 -0500

    fixed typo on withProviders

--- src/helpers/withProviders/index.tsx
+++ src/helpers/withProviders/index.tsx
@@ -3,5 +3,9 @@ import React from 'react';
 export type ProviderComponent = React.ComponentType<React.PropsWithChildren<any>>;
 
 export const withProviders = (...providers: ProviderComponent[]): ProviderComponent => {
-  return ({ children }) => providers.reduceRight((childThree, Provider: ProviderComponent) => React.createElement(Provider, null, childThree), children);
+  return ({ children }) =>
+    providers.reduceRight(
+      (childTree, Provider: ProviderComponent) => React.createElement(Provider, null, childTree),
+      children,
+    );
 };
